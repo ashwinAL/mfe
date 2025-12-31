93b7ae1eca4b222385e28e7582140a7f0c06e144
Ashwin Gattani <ashwin.gattani@accionlabs.com>
2025-12-31 15:38:25 +0530

    updated container app title

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default function App() {
   return (
     <div>
-      <h1>Primary Container App with GitHub Pages</h1>
+      <h1>Primary Container App with GitHub Pages latest</h1>
       <hr />
       <MarketingApp />
     </div>
